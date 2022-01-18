cca55aea5ca552a34e9a98f017846beb0bcae917
Paul Bastian <paul.bastian@bdr.de>
2022-01-18 11:14:23 +0100

    working cli loop
    Signed-off-by: Paul Bastian <paul.bastian@bdr.de>

--- indy-did-driver/src/main.rs
+++ indy-did-driver/src/main.rs
@@ -1,16 +1,18 @@
 use futures_executor::block_on;
 use indy_vdr::pool::{LocalPool, Pool, PoolBuilder, PoolTransactions, PreparedRequest, RequestResult, TimingResult};
-use std::{fmt, fs, io, process};
+use std::{fs, io, process};
 use std::collections::HashMap;
 use std::ops::Add;
-use indy_didresolver::did;
 use indy_vdr::common::error::VdrResult;
 use indy_vdr::pool::helpers::perform_ledger_request;
-use indy_didresolver::did::{Did, did_parse};
+use indy_didresolver::did::{did_parse, Did};
 use indy_didresolver::did_document::{DidDocument, Ed25519VerificationKey2018};
 use indy_didresolver::responses::GetNymResult;
 use indy_vdr::utils::did::DidValue;
 use serde_json::Value;
+use indy_didresolver::error::{DidIndyError, DidIndyResult};
+use rouille::Request;
+use rouille::Response;
 
 struct Ledger {
     name: String,
@@ -22,8 +24,7 @@ struct Ledger {
 
 
 fn main() {
-
-    let mut ledgers : HashMap<String, Ledger> = HashMap::new();
+    let mut ledgers: HashMap<String, Ledger> = HashMap::new();
     let entries = fs::read_dir("./genesis-files");
     match entries {
         Ok(entries) => {
@@ -52,43 +53,52 @@ fn main() {
 
     println!("Successfully imported {} Indy Ledgers from Files", ledgers.len());
 
-    println!("Please provide the requested DID...");
+    loop {
+        println!("Please provide the requested DID...");
 
-    let mut request = String::new();
-    io::stdin().read_line(&mut request).expect("Failed to read line");
-    //request.push_str("did:indy:idunion:BDrEcHc8Tb4Lb2VyQZWEDE");
+        let mut request = String::new();
+        io::stdin().read_line(&mut request).expect("Failed to read line");
 
-    let mut ledger_data = String::new();
+        process_request(&ledgers, request);
+    }
 
-    match did::did_parse(request.trim()){
+}
+
+fn process_request(ledgers: &HashMap<String, Ledger>, request: String) -> String {
+
+    let did = match did_parse(request.trim()){
         Ok(did) => {
-            match ledgers.get(&did.namespace) {
-                Some(ledger) => {
-                    let pool = &ledger.pool;
-                    let builder = pool.get_request_builder();
-                    let did_value = DidValue::new(&did.id, Option::None);
-                    let request = builder.build_get_nym_request(Option::None, &did_value).unwrap();
-                    let (result, _timing) = block_on(request_transaction(pool, request)).unwrap();
-                    match result {
-                        RequestResult::Reply(data) => {
-                            ledger_data = data;
-                        }
-                        RequestResult::Failed(error) => {
-                            println!("Error requesting DID from ledger, {}", error.to_string());
-                            process::exit(1);
-                        }
-                    }
-                }
-                None => {
-                    println!("Requested Indy Namespace \"{}\" unknown", &did.namespace);
-                    process::exit(1);
-                }
-            }
+            did
         },
         Err(DidIndyError) => {
             panic!(DidIndyError);
         }
-    }
+    };
+
+    let ledger= match ledgers.get(&did.namespace) {
+        Some(ledger) => {
+            ledger
+        }
+        None => {
+            println!("Requested Indy Namespace \"{}\" unknown", &did.namespace);
+            panic!(DidIndyError);
+        }
+    };
+
+    let pool = &ledger.pool;
+    let builder = pool.get_request_builder();
+    let did_value = DidValue::new(&did.id, Option::None);
+    let request = builder.build_get_nym_request(Option::None, &did_value).unwrap();
+    let (result, _timing) = block_on(request_transaction(pool, request)).unwrap();
+    let ledger_data = match result {
+        RequestResult::Reply(data) => {
+            data
+        }
+        RequestResult::Failed(error) => {
+            println!("Error requesting DID from ledger, {}", error.to_string());
+            panic!(DidIndyError);
+        }
+    };
 
     println!("Request successful: {}", ledger_data);
     let v: Value = serde_json::from_str(&ledger_data).unwrap();
@@ -98,21 +108,14 @@ fn main() {
     let get_nym: GetNymResult = serde_json::from_str(data.as_str().unwrap()).unwrap();
     println!("get_nym: {:?}", get_nym);
 
-    let did_document = DidDocument {
-        id: "did:indy:idunion:".to_string().add(&get_nym.dest.to_string()),
-        verification_method: vec![Ed25519VerificationKey2018 {
-            id: "did:indy:idunion:".to_string().add(&get_nym.dest.to_string()).add("#keys-1"),
-            type_: "Ed25519VerificationKey2018".to_string(),
-            controller: "did:indy:idunion:".to_string() + &get_nym.dest.to_string(),
-            public_key_base58: get_nym.verkey,
-        }],
-        authentication: vec![
-            "did:indy:idunion".to_string() + &get_nym.dest.to_string().add("#keys-1")
-        ]
-    };
+    let did_document = DidDocument::new(&did.namespace,&get_nym.dest,&get_nym.verkey);
+    let json = did_document.to_string();
 
-    let json = serde_json::to_string_pretty(&did_document).unwrap();
     println!("DID Document: {}", json);
+
+    return json
+
+
 
 }
 
--- indy-didresolver/src/did_document.rs
+++ indy-didresolver/src/did_document.rs
@@ -1,9 +1,10 @@
+use std::process::id;
 use serde::{Serialize};
 
 #[derive(Serialize, PartialEq, Debug)]
-pub struct DidDocument {
+pub struct DidDocumentJson {
     pub id: String,
-    #[serde(rename = "verification_method")]
+    #[serde(rename = "verificationMethod")]
     pub verification_method: Vec<Ed25519VerificationKey2018>,
     pub authentication: Vec<String>,
 }
@@ -16,4 +17,38 @@ pub struct Ed25519VerificationKey2018 {
     pub controller: String,
     #[serde(rename = "publicKeyBase58")]
     pub public_key_base58: String,
+}
+
+pub struct DidDocument {
+    namespace : String,
+    id : String,
+    verkey : String,
+}
+
+impl DidDocument {
+    pub fn new(namespace: &str, id : &str, verkey : &str) -> Self {
+        DidDocument {
+            namespace: namespace.to_string(),
+            id: id.to_string(),
+            verkey: verkey.to_string(),
+        }
+    }
+
+    pub fn to_string(&self) -> String{
+        let did_document = DidDocumentJson {
+            id: format!("did:indy:{}:{}", self.namespace, self.id),
+            verification_method: vec![Ed25519VerificationKey2018 {
+                id: format!("did:indy:{}:{}#keys-1", self.namespace, self.id),
+                type_: format!("Ed25519VerificationKey2018"),
+                controller: format!("did:indy:{}:{}", self.namespace, self.id),
+                public_key_base58: format!("{}",self.verkey),
+            }],
+            authentication: vec![
+                format!("did:indy:{}:{}#keys-1", self.namespace, self.id),
+            ]
+        };
+
+        serde_json::to_string_pretty(&did_document).unwrap()
+    }
+
 }
