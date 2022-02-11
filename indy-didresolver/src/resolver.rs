8743ca5294666cad6d2a7aae458b7e9c01971a40
Dominic Wörner <dom.woe@gmail.com>
2022-02-11 10:12:19 +0100

    introduce fn build_request
    Signed-off-by: Dominic Wörner <dom.woe@gmail.com>

--- indy-didresolver/src/resolver.rs
+++ indy-didresolver/src/resolver.rs
@@ -6,11 +6,11 @@ use serde_json::Value;
 
 use super::did::{did_parse, LedgerObject};
 use super::did_document::DidDocument;
-use super::error::{DidIndyError};
-use super::responses::{GetNymResult};
+use super::error::DidIndyError;
+use super::responses::GetNymResult;
 
 use indy_vdr::common::error::VdrResult;
-use indy_vdr::ledger::constants::{GET_NYM};
+use indy_vdr::ledger::constants::GET_NYM;
 use indy_vdr::ledger::identifiers::{CredentialDefinitionId, SchemaId};
 use indy_vdr::pool::helpers::perform_ledger_request;
 use indy_vdr::pool::{Pool, PreparedRequest, RequestResult, TimingResult};
@@ -41,23 +41,53 @@ impl<T: Pool> Resolver<T> {
         // The path variable identifies the requested ledger object
         // If there is no path, then we return a DID document
         let path = if did_url.path_abempty != "" {
-            Some(did_url.path_abempty)
+            Some(did_url.path_abempty.as_str())
         } else {
             None
         };
 
         println!("{:?}", path);
 
-        let builder = self.pool.get_request_builder();
         let did_value = DidValue::new(&did.id, Option::None);
 
+        let request = self.build_request(&did_value, path)?;
+
+        let ledger_data = handle_request(&self.pool, &request)?;
+
+        let v: Value = serde_json::from_str(&ledger_data).unwrap();
+        println!("result: {:?}", v);
+        let data: &Value = &v["result"]["data"];
+        println!("data: {:?}", data);
+        if *data == Value::Null {
+            return Err(DidIndyError);
+        }
+        let result = match request.txn_type.as_str() {
+            GET_NYM => {
+                let get_nym_result: GetNymResult =
+                    serde_json::from_str(data.as_str().unwrap()).unwrap();
+                let did_document =
+                    DidDocument::new(&did.namespace, &get_nym_result.dest, &get_nym_result.verkey);
+                did_document.to_string()
+            }
+            _ => data.to_string(),
+        };
+
+        Ok(result)
+    }
+
+    fn build_request(
+        &self,
+        did: &DidValue,
+        path: Option<&str>,
+    ) -> Result<PreparedRequest, DidIndyError> {
+        let builder = self.pool.get_request_builder();
         let request = match path {
-            Some(path) => match LedgerObject::from_str(path.as_str())? {
+            Some(path) => match LedgerObject::from_str(path)? {
                 LedgerObject::ClaimDef(claim_def) => builder
                     .build_get_cred_def_request(
                         Option::None,
                         &(CredentialDefinitionId::new(
-                            &did_value.to_unqualified(),
+                            &did.to_unqualified(),
                             &SchemaId(claim_def.schema_id),
                             "CL",
                             &claim_def.name,
@@ -74,34 +104,11 @@ impl<T: Pool> Resolver<T> {
                 LedgerObject::RevRegEntry(_) => unimplemented!("Arm not implemented yet"),
             },
             None => builder
-                .build_get_nym_request(Option::None, &did_value)
+                .build_get_nym_request(Option::None, &did)
                 .unwrap(),
         };
-
-        let ledger_data = handle_request(&self.pool, &request)?;
-
-        let v: Value = serde_json::from_str(&ledger_data).unwrap();
-        println!("result: {:?}", v);
-        let data: &Value = &v["result"]["data"];
-        println!("data: {:?}", data);
-        if *data == Value::Null {
-            return Err(DidIndyError);
-        }
-        let result = match request.txn_type.as_str() {
-            GET_NYM => {
-                let get_nym_result: GetNymResult = serde_json::from_str(data.as_str().unwrap()).unwrap();
-                let did_document = DidDocument::new(&did.namespace, &get_nym_result.dest, &get_nym_result.verkey);
-                did_document.to_string()
-            }
-            _ => {
-                data.to_string()
-            }
-        };
-
-        Ok(result)
-        
+        Ok(request)
     }
-
 }
 
 fn handle_request<T: Pool>(pool: &T, request: &PreparedRequest) -> Result<String, DidIndyError> {
